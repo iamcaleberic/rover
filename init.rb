d619cf5c3633b72fe30fdd62348882f0aaa4b2bf
Eric Caleb <calebme@mail.com>
2016-08-05 13:37:11 +0100

    Intialize methods and add rspec

---
+++ init.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require_relative('lib/rover')
+
+puts
+puts "Enter info of rover you want to purchase"
+
+puts
+
+print "Make:"
+make = gets.chomp 
+
+print "Year:"
+year = gets.chomp
+
+print "Color:"
+color = gets.chomp
+
+print "Planet:"
+planet = gets.chomp
+
+rover = Rover.new(:make => make , :year => year , :color => color , :planet => planet)
+
+puts
+puts "I understand you want to purchase: #{rover.full_name} ."
+
+puts
---
+++ lib/rover.rb
@@ -0,0 +1,21 @@
+class Rover
+	attr_accessor :make , :year , :color , :planet
+	attr_reader :wheels
+	attr_writer :door
+
+	def initialize(options={})
+		self.make = options[:make] || "Calebs Rover"
+		self.year = (options[:year] || 2139).to_i
+		self.color = options[:color] || "unknown" 
+		self.planet = options[:planet] || "Caleb's planet"
+		@wheels = 4
+	end
+
+	def self.color
+		['blue' , 'black' , 'red ' , 'green']
+	end
+
+	def full_name
+		"#{self.year.to_s} #{self.make} (#{self.color}) for travel on #{self.planet}"
+	end
+end
