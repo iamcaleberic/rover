b5505cbf3753668f52843f7869ff130145d325a6
Eric Caleb <calebme@mail.com>
2016-08-17 09:09:47 +0100

    Modified doubles spec added specs

---
+++ spec/doubles_spec.rb
@@ -0,0 +1,20 @@
+describe "Doubles" do 
+
+    it "allows stubbing methods" do 
+    end
+
+    it "allows stubbing a response with a block " do
+    end
+
+    it "allows stubbing response with #and_return" do
+    end
+
+    it "allows stubbing multiple methods with hash syntax" do
+    end
+
+    it "allows stubbing with a hash argument to #double" do
+    end
+
+    it "allows stubbing multiple responses with #and_return"
+
+end
